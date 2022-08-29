97a97921b4eed78bd47845ee4b90b7a6fa4577bc
Istvan <palocsayi@gmail.com>
2022-08-29 19:58:08 +0200

    meetingrooms - do while loop kiegeszites, hogy ne szakadjon meg a program futasa amig ki nem lep a felhasznalo; plusz menupont a szobak hozzaadasahoz

--- meetingrooms/src/main/java/meetingrooms/MeetingRoomController.java
+++ meetingrooms/src/main/java/meetingrooms/MeetingRoomController.java
@@ -8,6 +8,8 @@ public class MeetingRoomController {
 	Scanner sc = new Scanner(System.in);
 	Office office = new Office();
 
+	private boolean toContinue = true;
+
 
 	public static void main(String[] args) {
 		MeetingRoomController meetingRoomController = new MeetingRoomController();
@@ -15,18 +17,24 @@ public class MeetingRoomController {
 	}
 
 	private void runMenu() {
-		System.out.print("Welcome! Enter the number of meeting rooms you want to add: ");
-		int meetingRoomsToAdd = sc.nextInt();
-		sc.nextLine();
-		addMeetingRooms(meetingRoomsToAdd);
-		printMenu();
-		System.out.print("Enter your choose: ");
-		int userChoose = sc.nextInt();
-		sc.nextLine();
-		handleChoose(userChoose);
+		System.out.println("Welcome!");
+		do {
+			if (office.getMeetingRooms().isEmpty()) {
+				addMeetingRooms();
+			}
+			printMenu();
+			System.out.print("Enter your choose: ");
+			int userChoose = sc.nextInt();
+			sc.nextLine();
+			handleChoose(userChoose);
+		}
+		while (toContinue);
 	}
 
-	private void addMeetingRooms(int meetingRoomsToAdd) {
+	private void addMeetingRooms() {
+		System.out.println("Enter the amount of meeting rooms you want to add:");
+		int meetingRoomsToAdd = sc.nextInt();
+		sc.nextLine();
 		for(int i = 0; i < meetingRoomsToAdd; i++) {
 			System.out.print("Enter the name of the meeting room: ");
 			String name = sc.nextLine();
@@ -50,37 +58,59 @@ public class MeetingRoomController {
 				5. Search for the exact name
 				6. Search for a part of the name
 				7. Search based on area
-				8. Exit""");
+				8. Add more meeting rooms
+				9. Exit""");
 	}
 
 	private void handleChoose(int choose) {
 		switch (choose) {
-			case 1 -> printMeetingRoomNames(office.getMeetingRooms());
-			case 2 -> printMeetingRoomNames(office.getMeetingRoomsInReverseOrder());
+			case 1 -> {
+				System.out.println("\nList of meeting rooms in order:");
+				printMeetingRoomNames(office.getMeetingRooms());
+				System.out.println();
+			}
+			case 2 -> {
+				System.out.println("\nList of meeting rooms in reverse order:");
+				printMeetingRoomNames(office.getMeetingRoomsInReverseOrder());
+				System.out.println();
+			}
 			case 3 -> {
-				System.out.println("Type 1 to list every odd numbered meeting room, and 2 to every even");
+				System.out.println("\nType 1 to list every odd numbered meeting room, and 2 to every even");
 				int evenOrOdd = sc.nextInt();
 				sc.nextLine();
+				System.out.println("\nList of every second meeting room:");
 				printMeetingRoomNames(office.getEverySecondMeetingRoom(evenOrOdd));
+				System.out.println();
 			}
-			case 4 -> printMeetingRooms(office.getMeetingRooms());
+			case 4 -> {
+				System.out.println("\nList of the meeting rooms in details:");
+				printMeetingRooms(office.getMeetingRooms());
+				System.out.println();
+			}
 			case 5 -> {
-				System.out.println("Enter the name of the meeting room you:");
+				System.out.println("\nEnter the name of the meeting room you:");
 				String nameToSearch = sc.nextLine();
 				printMeetingRoom(office.getMeetingRoomWithGivenName(nameToSearch));
+				System.out.println();
 			}
 			case 6 -> {
-				System.out.println("Enter a part of a name you want to search for:");
+				System.out.println("\nEnter a part of a name you want to search for:");
 				String namePartToSearch = sc.nextLine();
 				printMeetingRooms(office.getMeetingRoomsWithGivenNamePart(namePartToSearch));
+				System.out.println();
 			}
 			case 7 -> {
-				System.out.println("Enter the minimum area to list meeting rooms:");
+				System.out.println("\nEnter the minimum area to list meeting rooms:");
 				int areaToFilter = sc.nextInt();
 				sc.nextLine();
 				printMeetingRooms(office.getMeetingRoomsWithAreaLargerThan(areaToFilter));
+				System.out.println();
 			}
-			case 8 -> System.out.println("auf Wiedersehen");
+			case 8 -> addMeetingRooms();
+			case 9 -> {
+				System.out.println("auf Wiedersehen");
+				toContinue = false;
+			}
 			default -> System.out.println("Invalid option");
 		}
 	}
@@ -98,9 +128,9 @@ public class MeetingRoomController {
 	}
 
 	private void printMeetingRoom(MeetingRoom meetingRoom) {
-		System.out.println("Name: " + meetingRoom.getName() +
-				"width: " + meetingRoom.getWidth() +
-				"length: " + meetingRoom.getLength() +
+		System.out.println("Name: " + meetingRoom.getName() + "\n" +
+				"width: " + meetingRoom.getWidth() + "\n" +
+				"length: " + meetingRoom.getLength() + "\n" +
 				"area: " + meetingRoom.getArea());
 	}
 }
